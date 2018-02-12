12cf2cb526ed6b2d12106894ca735cf81ba432ff
Adrian Neatu <adrian.neatu@outlook.com>
2018-02-12 06:24:54 +0200

    billing alert

--- cloudwatch-billing.tf
+++ cloudwatch-billing.tf
@@ -1,18 +1,32 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
   access_key = ""
   secret_key = ""
+}
+
+resource "aws_sns_topic" "sms_notification" {
+  name = "sms-notification-topic"
+}
+
+resource "aws_sns_topic_subscription" "billing_sms" {
+  topic_arn = "${aws_sns_topic.sms_notification.arn}"
+  protocol  = "sms"
+  endpoint  = "+40760080301"
 }
 
 resource "aws_cloudwatch_metric_alarm" "billingAlert" {
   alarm_name                = "billingAlert"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "2"
+  evaluation_periods        = "1"
   metric_name               = "EstimatedCharges"
   namespace                 = "AWS/Billing"
-  period                    = "3600" // 1 hour
+  period                    = "3600"                          // 1 hour
   statistic                 = "Maximum"
   threshold                 = "10"
-  alarm_description         = "This metric monitors charges"
+  alarm_description         = "Sends SMS when billing is above 10$"
+  dimensions {
+    Currency = "USD"
+  }
   insufficient_data_actions = []
+  alarm_actions = ["${aws_sns_topic.sms_notification.arn}"]
 }
